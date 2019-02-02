44239b293f1874b2c2aacd3096c58941d1560f74
Raika <Raika.Maftoon@gmail.com>
2019-02-02 08:33:59 +0330

    init project

---
+++ src/index.ts
@@ -0,0 +1,111 @@
+import * as datefns from "date-fns";
+import * as jalaali from "jalaali-js";
+
+class RDate {
+    public date: Date;
+
+    constructor(input: number[] | string | Date = new Date()) {
+        if (Array.isArray(input)) {
+            const gd = jalaali.toGregorian(input[0], input[1], input[2]);
+            this.date = new Date(gd.gy, gd.gm - 1, gd.gd);
+        } else if (typeof input === "string") {
+            this.date = datefns.parse(input);
+        } else {
+            this.date = input;
+        }
+    }
+
+    public addDays(days: number): RDate {
+        return new RDate(datefns.addDays(this.date, days));
+    }
+
+    public subDays(days: number): RDate {
+        return new RDate(datefns.subDays(this.date, days));
+    }
+
+    public addMonths(months: number): RDate {
+        return new RDate(datefns.addMonths(this.date, months));
+    }
+
+    public subMonths(months: number): RDate {
+        return new RDate(datefns.subMonths(this.date, months));
+    }
+
+    public addYears(years: number): RDate {
+        return new RDate(datefns.addYears(this.date, years));
+    }
+
+    public subYears(years: number): RDate {
+        return new RDate(datefns.subYears(this.date, years));
+    }
+
+    public startOfMonth(): RDate {
+        return new RDate(datefns.startOfMonth(this.date));
+    }
+
+    public endOfMonth(): RDate {
+        return new RDate(datefns.endOfMonth(this.date));
+    }
+
+    public startOfMonthJalali(): RDate {
+        const [jy, jm, _] = this.getJalali();
+        return new RDate([jy, jm, 1]);
+    }
+
+    public endOfMonthJalali(): RDate {
+        const [jy, jm, _] = this.getJalali();
+        const ml = jalaali.jalaaliMonthLength(jy, jm);
+        return new RDate([jy, jm, ml]);
+    }
+
+    public isAfter(date: Date): boolean {
+        return datefns.isAfter(this.date, date);
+    }
+
+    public isBefore(date: Date): boolean {
+        return datefns.isBefore(this.date, date);
+    }
+
+    public format(frmt: string): string {
+        return datefns.format(this.date, frmt);
+    }
+
+    public formatJalali(frmt: string): string {
+        const [jy, jm, jd] = this.getJalali();
+        frmt = frmt.replace("YYYY", `${jy}`);
+        frmt = frmt.replace("YYY", `${jy}`);
+        frmt = frmt.replace("YY", `${jy}`.slice(2));
+        frmt = frmt.replace("MM", this.zeroLeading(`${jm}`));
+        frmt = frmt.replace("M", `${jm}`);
+        frmt = frmt.replace("DD", this.zeroLeading(`${jd}`));
+        frmt = frmt.replace("D", `${jd}`);
+
+        const hour = this.date.getHours();
+        const minute = this.date.getMinutes();
+        const second = this.date.getSeconds();
+
+        frmt = frmt.replace("hh", this.zeroLeading(`${hour}`));
+        frmt = frmt.replace("h", `${hour}`);
+
+        frmt = frmt.replace("mm", this.zeroLeading(`${minute}`));
+        frmt = frmt.replace("m", `${minute}`);
+
+        frmt = frmt.replace("ss", this.zeroLeading(`${second}`));
+        frmt = frmt.replace("s", `${second}`);
+        return frmt;
+    }
+
+    private getJalali(): number[] {
+        const jd = jalaali.toJalaali(this.date);
+        return [jd.jy, jd.jm, jd.jd];
+    }
+
+    private zeroLeading(str: string) {
+        if (str && str.length === 1) {
+            return `0${str}`;
+        }
+        return str;
+    }
+}
+
+export default RDate;
---
+++ test/rdate.test.ts
@@ -0,0 +1,51 @@
+import { expect } from "chai";
+import "mocha";
+import RDate from "../src";
+
+describe("construnctor", () => {
+    it("should return correct Date from string", () => {
+        expect(new RDate("2019-01-01").date).to.eql(new Date(2019, 0, 1));
+    });
+    it("should return correct Date from date", () => {
+        expect(new RDate(new Date(2019, 0, 1)).date).to.eql(new Date(2019, 0, 1));
+    });
+    it("should return correct Date from array number", () => {
+        expect(new RDate([1397, 10, 11]).date).to.eql(new Date(2019, 0, 1));
+    });
+});
+
+describe("startOfMonth", () => {
+    it("should return start of the month gerogian", () => {
+        const d = new RDate("2019-01-15").startOfMonth();
+        expect(d.date).to.eql(new Date(2019, 0, 1));
+    });
+
+    it("should return start of the month jalali", () => {
+        const d = new RDate([1397, 1, 15]).startOfMonthJalali();
+        expect(d.date).to.eql(new RDate([1397, 1, 1]).date);
+    });
+});
+
+describe("endOfMonth", () => {
+    it("should return end of the month georgian", () => {
+        const d = new RDate("2019-01-15").endOfMonth();
+        expect(d.date.toLocaleDateString()).to.be.equal(new Date(2019, 0, 31).toLocaleDateString());
+    });
+
+    it("should return end of the month jalali", () => {
+        const d = new RDate([1397, 1, 15]).endOfMonthJalali();
+        expect(d.date).to.eql(new RDate([1397, 1, 31]).date);
+    });
+});
+
+describe("format", () => {
+    it("should return formated gerogian date", () => {
+        const d = new RDate("2019-01-15");
+        expect(d.format("MM/DD/YYYY")).to.be.equal("01/15/2019");
+    });
+
+    it("should return formated jalali date", () => {
+        const d = new RDate([1397, 1, 1]);
+        expect(d.formatJalali("YYYY/MM/DD")).to.be.equal("1397/01/01");
+    });
+});
